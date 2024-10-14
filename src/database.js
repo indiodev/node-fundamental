161a9f043d03bb4f8d73b4e694ef7d8197687ea5
indiodev <jhollyfer.fr@gmail.com>
2024-10-14 06:04:22 -0500

    config database

--- src/database.js
+++ src/database.js
@@ -1,16 +1,16 @@
 export class Database {
-  database = {};
+  #database = {};
 
   select(table) {
-    const data = this.database[table] ?? [];
+    const data = this.#database[table] ?? [];
     return data;
   }
 
   insert(table, data) {
     if (Array.isArray(this.database[table])) {
-      this.database[table].push(data);
+      this.#database[table].push(data);
     } else {
-      this.database[table] = [data];
+      this.#database[table] = [data];
     }
 
     return data;
