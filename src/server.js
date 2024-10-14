3827eb58397883ec4984595bdbb53d028af5b1e1
indiodev <jhollyfer.fr@gmail.com>
2024-10-14 06:03:31 -0500

    config database

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import { createServer } from "node:http";
 import { METHOD, ROUTE, USER_LIST } from "./constant.js";
+import { Database } from "./database.js";
 import { _json } from "./middlewares/json.js";
+
+const database = new Database();
 
 const server = createServer(async function (request, response) {
   const { method, url } = request;
@@ -8,18 +11,22 @@ const server = createServer(async function (request, response) {
   await _json(request, response);
 
   if (method === METHOD.GET && url === ROUTE.USERS) {
+    const users = database.select("users");
     return response
       .setHeader("Content-Type", "application/json")
-      .end(JSON.stringify(USER_LIST, null, 2));
+      .end(JSON.stringify(users, null, 2));
   }
 
   if (method === METHOD.POST && url === ROUTE.USERS) {
     const { name, email } = request.body;
-    USER_LIST.push({
+
+    const user = {
       id: USER_LIST.length + 1,
       name,
       email,
-    });
+    };
+
+    database.insert("users", user);
     return response.writeHead(201).end();
   }
 
