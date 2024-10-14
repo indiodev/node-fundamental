a467518235a6d5721ce8148df834e262f77bfd5f
indiodev <jhollyfer.fr@gmail.com>
2024-10-14 06:19:59 -0500

    persist data on database

--- src/database.js
+++ src/database.js
@@ -1,5 +1,22 @@
+import { readFile, writeFile } from "node:fs/promises";
+
+const path = new URL("../db.json", import.meta.url);
 export class Database {
   #database = {};
+
+  constructor() {
+    readFile(path, "utf8")
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  }
+
+  #persist() {
+    writeFile(path, JSON.stringify(this.#database, null, 2));
+  }
 
   select(table) {
     const data = this.#database[table] ?? [];
@@ -7,11 +24,13 @@ export class Database {
   }
 
   insert(table, data) {
-    if (Array.isArray(this.database[table])) {
+    if (Array.isArray(this.#database[table])) {
       this.#database[table].push(data);
     } else {
       this.#database[table] = [data];
     }
+
+    this.#persist();
 
     return data;
   }
